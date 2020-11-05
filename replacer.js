bde2b59b84869bb3a983481c2b963ebebb1a1536
Sergei Savelev (ssav) <ssav@yandex-team.ru>
2020-11-05 03:46:40 +0300

    fix: add case with block + mod

--- replacer.js
+++ replacer.js
@@ -1,19 +1,22 @@
-const makeRegExpStr = (s) => new RegExp(`(\\.)(${s})(([-_]\\w+)|\\W|$)`, 'g');
+const makeRegExpStr = (s) => new RegExp(`(${s})(([-_]\\w+)|\\W|$)`, 'g');
 
 const replacer = (selector, blocks, { suffix = '', prefix = '' }) => {
     const regExps = blocks.map((block) => makeRegExpStr(block));
+    let [first, ...classParts] = selector.split('.');
 
     for (let blockRegExp of regExps) {
-        if (!blockRegExp.test(selector)) continue;
+        classParts = classParts.map((classPart) => {
+            if (!blockRegExp.test(classPart)) return classPart;
 
-        selector = selector.replace(blockRegExp, (_, before, block, after) => {
-            return `${before}${prefix && `${prefix}-`}${block}${
-                suffix && `-${suffix}`
-            }${after}`;
+            return classPart.replace(blockRegExp, (_, block, after) => {
+                return `${prefix && `${prefix}-`}${block}${
+                    suffix && `-${suffix}`
+                }${after}`;
+            });
         });
     }
 
-    return selector;
+    return [first, ...classParts].join('.');
 };
 
 module.exports = replacer;
--- replacer.test.js
+++ replacer.test.js
@@ -1,30 +1,54 @@
 const { it, describe } = require('@jest/globals');
 const replacer = require('./replacer');
 
-const blocks = ['Button', 'Select', 'Divider'];
+const blocks = ['Button', 'Select', 'Divider', 'Popup2'];
 
 describe('replacer', () => {
     it('base', () => {
-        expect(replacer('.Button', blocks, { suffix: 'suffix' })).toBe('.Button-suffix');
-    });
-
-    it('element', () => {
-        expect(replacer('.Button-Text .Select .Other', blocks, { suffix: 'suffix' })).toBe(
-            '.Button-suffix-Text .Select-suffix .Other'
+        expect(replacer('.Button', blocks, { suffix: 'suffix' })).toBe(
+            '.Button-suffix'
         );
     });
 
+    it('element', () => {
+        expect(
+            replacer('.Button-Text .Select .Other', blocks, {
+                suffix: 'suffix',
+            })
+        ).toBe('.Button-suffix-Text .Select-suffix .Other');
+    });
+
     it('mod', () => {
-        expect(replacer('.Button_mod .Button_modValue.Button_mod_Value .Other', blocks, { suffix: 'suffix' })).toBe(
+        expect(
+            replacer(
+                '.Button_mod .Button_modValue.Button_mod_Value .Other',
+                blocks,
+                { suffix: 'suffix' }
+            )
+        ).toBe(
             '.Button-suffix_mod .Button-suffix_modValue.Button-suffix_mod_Value .Other'
         );
     });
 
     it('base prefix', () => {
-        expect(replacer('.Button', blocks, { prefix: 'prefix' })).toBe('.prefix-Button');
+        expect(replacer('some.Button', blocks, { prefix: 'prefix' })).toBe(
+            'some.prefix-Button'
+        );
     });
 
     it('attributes and pseudo', () => {
-        expect(replacer('.Button[ab] .Select::before', blocks, { prefix: 'prefix' })).toBe('.prefix-Button[ab] .prefix-Select::before');
+        expect(
+            replacer('.Button[ab] .Select::before', blocks, {
+                prefix: 'prefix',
+            })
+        ).toBe('.prefix-Button[ab] .prefix-Select::before');
+    });
+
+    it('Block + mod concated', () => {
+        expect(
+            replacer('.Popup2.Popup2_view_default', blocks, {
+                prefix: 'prefix',
+            })
+        ).toBe('.prefix-Popup2.prefix-Popup2_view_default');
     });
 });
